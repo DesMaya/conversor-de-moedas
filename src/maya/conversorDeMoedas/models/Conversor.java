07568d837ddde592a42c26388e6576edde52806d
Maya <mayaflorentinodasilva@gmail.com>
2024-10-02 16:17:40 -0300

    Removendo comentários desnecessários

--- src/maya/conversorDeMoedas/models/Conversor.java
+++ src/maya/conversorDeMoedas/models/Conversor.java
@@ -7,7 +7,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-//https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT
 
 public class Conversor {
     private Conversao conversao;
