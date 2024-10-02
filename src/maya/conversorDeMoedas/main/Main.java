993a9a43b182f0a6496a9e7eb6490d2d4b563cb2
Maya <mayaflorentinodasilva@gmail.com>
2024-10-02 16:00:20 -0300

    Criado teste para a classe conversor com valores fixos

--- src/maya/conversorDeMoedas/main/Main.java
+++ src/maya/conversorDeMoedas/main/Main.java
@@ -1,7 +1,10 @@
 package maya.conversorDeMoedas.main;
 
+import maya.conversorDeMoedas.models.Conversor;
+
 public class Main {
     public static void main(String[] args) {
-
+        Conversor conversor = new Conversor();
+        conversor.converter("BRL", "USD", 100);
     }
 }
