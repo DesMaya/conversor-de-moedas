2308da1f4d16f5930886c85c5aca25c072a825c9
Maya <mayaflorentinodasilva@gmail.com>
2024-10-02 17:08:20 -0300

    Criando a mensagem padrão do menu de opções

--- src/maya/conversorDeMoedas/main/Main.java
+++ src/maya/conversorDeMoedas/main/Main.java
@@ -2,13 +2,40 @@ package maya.conversorDeMoedas.main;
 
 import maya.conversorDeMoedas.models.Conversor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Conversor conversor = new Conversor();
+        var opcao = 0;
+        var menssagemOpcoes = """
+        *****************************************************
+        
+        1) Dólar Americano => Real Brasileiro
+        2) Real Brasileiro => Dólar Americano
+        3) Euro => Real Brasileiro
+        4) Real Brasileiro => Euro
+        5) Dolar Americano => Euro
+        6) Euro => Dolar Americano
+        7) Sair
+        
+        Escolha uma opção válida: 
+        *****************************************************
+        """;
+
+        System.out.println("""
+        
+        Bem vindo/a ao Conversor de Moedas!
+        
+        """);
+
         try {
-            Conversor conversor = new Conversor();
             conversor.converter("BRL", "USD", 100);
+            System.out.println("Valor convertido: " + conversor.getQuantia());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
     }
 }
