c8972cc8fa2ae44abe742d60740c7fbc1664be4a
Maya <mayaflorentinodasilva@gmail.com>
2024-10-02 15:46:55 -0300

    Criada a estrutura base HTTP request e response para buscar uma conversão na api + trativa de erros

--- src/maya/conversorDeMoedas/models/Conversor.java
+++ src/maya/conversorDeMoedas/models/Conversor.java
@@ -1,5 +1,35 @@
 package maya.conversorDeMoedas.models;
 
-public class Conversor {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+//https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT
 
+public class Conversor {
+    private Conversao conversao;
+
+    public void converter(String moeda_base, String moeda_alvo) {
+        var key = "05c0ca24299e9c5398f611e0";
+        var uri = URI.create(
+                "https://v6.exchangerate-api.com/v6/" +
+                key + "/pair/" +
+                moeda_alvo + "/" +
+                moeda_base + "/AMOUNT" );
+
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder()
+                .uri(uri)
+                .build();
+        try {
+            HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+            var json = response.body();
+            System.out.println(json);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Não foi possível encontrar esse par para conversão!");
+        }
+
+    }
 }
