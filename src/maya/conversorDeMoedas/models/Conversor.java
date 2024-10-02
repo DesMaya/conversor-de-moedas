d55a083a2effaeb925c99cd50f722893ded8d034
Maya <mayaflorentinodasilva@gmail.com>
2024-10-02 16:01:22 -0300

    Adição da quantia na requisição e gravando a resposta da api na classe Record Conversao

--- src/maya/conversorDeMoedas/models/Conversor.java
+++ src/maya/conversorDeMoedas/models/Conversor.java
@@ -1,5 +1,9 @@
 package maya.conversorDeMoedas.models;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,13 +14,15 @@ import java.net.http.HttpResponse;
 public class Conversor {
     private Conversao conversao;
 
-    public void converter(String moeda_base, String moeda_alvo) {
+    public void converter(String moeda_base, String moeda_alvo, double quantia) {
+
         var key = "05c0ca24299e9c5398f611e0";
         var uri = URI.create(
                 "https://v6.exchangerate-api.com/v6/" +
                 key + "/pair/" +
+                moeda_base + "/" +
                 moeda_alvo + "/" +
-                moeda_base + "/AMOUNT" );
+                quantia );
 
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder()
@@ -25,8 +31,11 @@ public class Conversor {
         try {
             HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-            var json = response.body();
-            System.out.println(json);
+
+            conversao = new Gson().fromJson(response.body(), Conversao.class);
+            //System.out.println(response.body());
+            //System.out.println(conversao);
+
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Não foi possível encontrar esse par para conversão!");
         }
