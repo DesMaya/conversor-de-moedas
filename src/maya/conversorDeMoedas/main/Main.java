700a26f4947d32246150e1e10d3c108f4925dc42
Maya <mayaflorentinodasilva@gmail.com>
2024-10-03 18:54:20 -0300

    Implementando as condicionais para atender cada uma das opções listadas

--- src/maya/conversorDeMoedas/main/Main.java
+++ src/maya/conversorDeMoedas/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Conversor conversor = new Conversor();
         var menssagemOpcoes = """
         *****************************************************
-        
+       \s
         1) Dólar Americano => Real Brasileiro
         2) Real Brasileiro => Dólar Americano
         3) Euro => Real Brasileiro
@@ -19,11 +19,10 @@ public class Main {
         5) Dolar Americano => Euro
         6) Euro => Dolar Americano
         7) Sair
-        
-        Escolha uma opção válida: 
+       \s
+        Escolha uma opção:\s
         *****************************************************
-        
-        """;
+       \s""";
 
         System.out.println("""
         
@@ -36,13 +35,59 @@ public class Main {
                 Scanner sc = new Scanner(System.in);
                 System.out.println(menssagemOpcoes);
                 var opcao = sc.nextInt();
+                var quantia = 0.0;
                 if (opcao == 7 ) break;
 
-                conversor.converter("BRL", "USD", 100);
-                System.out.println("Valor convertido: " + conversor.getQuantia());
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Digite o valor que deseja converter: ");
+                        quantia = sc.nextDouble();
+                        conversor.converter("USD", "BRL", quantia);
+                        conversor.mostraConversao();
+                        continue;
+
+                    case 2:
+                        System.out.println("Digite o valor que deseja converter: ");
+                        quantia = sc.nextDouble();
+                        conversor.converter("BRL", "USD", quantia);
+                        conversor.mostraConversao();
+                        continue;
+
+                    case 3:
+                        System.out.println("Digite o valor que deseja converter: ");
+                        quantia = sc.nextDouble();
+                        conversor.converter("EUR", "BRL", quantia);
+                        conversor.mostraConversao();
+                        continue;
+
+                    case 4:
+                        System.out.println("Digite o valor que deseja converter: ");
+                        quantia = sc.nextDouble();
+                        conversor.converter("BRL", "EUR", quantia);
+                        conversor.mostraConversao();
+                        continue;
+
+                    case 5:
+                        System.out.println("Digite o valor que deseja converter: ");
+                        quantia = sc.nextDouble();
+                        conversor.converter("USD", "EUR", quantia);
+                        conversor.mostraConversao();
+                        continue;
+
+                    case 6:
+                        System.out.println("Digite o valor que deseja converter: ");
+                        quantia = sc.nextDouble();
+                        conversor.converter("EUR", "USD", quantia);
+                        conversor.mostraConversao();
+                        continue;
+
+                    default:
+                        System.out.println("Digite uma opção válida!");
+                }
+
 
             } catch (InputMismatchException e) {
-                System.out.println("Opção inválida!");
+                System.out.println("Ops! Você digitou um valor inválido, tente novamente!");
             }
         }
 
